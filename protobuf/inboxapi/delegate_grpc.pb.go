f0b48169ddc48c75cedda553afb101df4ff575a8
Alexander Kirii <nwn.wert@gmail.com>
2024-09-04 11:23:37 +0500

    Store user delegation

--- protobuf/inboxapi/delegate_grpc.pb.go
+++ protobuf/inboxapi/delegate_grpc.pb.go
@@ -20,7 +20,9 @@ import (
 const _ = grpc.SupportPackageIsVersion9
 
 const (
-	Delegate_GetAllowedDaos_FullMethodName = "/inboxapi.Delegate/GetAllowedDaos"
+	Delegate_GetAllowedDaos_FullMethodName    = "/inboxapi.Delegate/GetAllowedDaos"
+	Delegate_StoreDelegated_FullMethodName    = "/inboxapi.Delegate/StoreDelegated"
+	Delegate_GetLastDelegation_FullMethodName = "/inboxapi.Delegate/GetLastDelegation"
 )
 
 // DelegateClient is the client API for Delegate service.
@@ -28,6 +30,8 @@ const (
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
 type DelegateClient interface {
 	GetAllowedDaos(ctx context.Context, in *emptypb.Empty, opts ...grpc.CallOption) (*GetAllowedDaosResponse, error)
+	StoreDelegated(ctx context.Context, in *StoreDelegatedRequest, opts ...grpc.CallOption) (*emptypb.Empty, error)
+	GetLastDelegation(ctx context.Context, in *GetLastDelegationRequest, opts ...grpc.CallOption) (*GetLastDelegationResponse, error)
 }
 
 type delegateClient struct {
@@ -48,11 +52,33 @@ func (c *delegateClient) GetAllowedDaos(ctx context.Context, in *emptypb.Empty, 
 	return out, nil
 }
 
+func (c *delegateClient) StoreDelegated(ctx context.Context, in *StoreDelegatedRequest, opts ...grpc.CallOption) (*emptypb.Empty, error) {
+	cOpts := append([]grpc.CallOption{grpc.StaticMethod()}, opts...)
+	out := new(emptypb.Empty)
+	err := c.cc.Invoke(ctx, Delegate_StoreDelegated_FullMethodName, in, out, cOpts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *delegateClient) GetLastDelegation(ctx context.Context, in *GetLastDelegationRequest, opts ...grpc.CallOption) (*GetLastDelegationResponse, error) {
+	cOpts := append([]grpc.CallOption{grpc.StaticMethod()}, opts...)
+	out := new(GetLastDelegationResponse)
+	err := c.cc.Invoke(ctx, Delegate_GetLastDelegation_FullMethodName, in, out, cOpts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // DelegateServer is the server API for Delegate service.
 // All implementations must embed UnimplementedDelegateServer
 // for forward compatibility.
 type DelegateServer interface {
 	GetAllowedDaos(context.Context, *emptypb.Empty) (*GetAllowedDaosResponse, error)
+	StoreDelegated(context.Context, *StoreDelegatedRequest) (*emptypb.Empty, error)
+	GetLastDelegation(context.Context, *GetLastDelegationRequest) (*GetLastDelegationResponse, error)
 	mustEmbedUnimplementedDelegateServer()
 }
 
@@ -65,6 +91,12 @@ type UnimplementedDelegateServer struct{}
 
 func (UnimplementedDelegateServer) GetAllowedDaos(context.Context, *emptypb.Empty) (*GetAllowedDaosResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method GetAllowedDaos not implemented")
+}
+func (UnimplementedDelegateServer) StoreDelegated(context.Context, *StoreDelegatedRequest) (*emptypb.Empty, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method StoreDelegated not implemented")
+}
+func (UnimplementedDelegateServer) GetLastDelegation(context.Context, *GetLastDelegationRequest) (*GetLastDelegationResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method GetLastDelegation not implemented")
 }
 func (UnimplementedDelegateServer) mustEmbedUnimplementedDelegateServer() {}
 func (UnimplementedDelegateServer) testEmbeddedByValue()                  {}
@@ -105,6 +137,42 @@ func _Delegate_GetAllowedDaos_Handler(srv interface{}, ctx context.Context, dec 
 	return interceptor(ctx, in, info, handler)
 }
 
+func _Delegate_StoreDelegated_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(StoreDelegatedRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(DelegateServer).StoreDelegated(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: Delegate_StoreDelegated_FullMethodName,
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(DelegateServer).StoreDelegated(ctx, req.(*StoreDelegatedRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _Delegate_GetLastDelegation_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(GetLastDelegationRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(DelegateServer).GetLastDelegation(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: Delegate_GetLastDelegation_FullMethodName,
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(DelegateServer).GetLastDelegation(ctx, req.(*GetLastDelegationRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 // Delegate_ServiceDesc is the grpc.ServiceDesc for Delegate service.
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
@@ -115,6 +183,14 @@ var Delegate_ServiceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "GetAllowedDaos",
 			Handler:    _Delegate_GetAllowedDaos_Handler,
+		},
+		{
+			MethodName: "StoreDelegated",
+			Handler:    _Delegate_StoreDelegated_Handler,
+		},
+		{
+			MethodName: "GetLastDelegation",
+			Handler:    _Delegate_GetLastDelegation_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
